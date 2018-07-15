49bca9e72d3fe05b68d5fbefa55b87fab3a8a6d7
zsonagy <gerzsonbigdata@gmail.com>
2018-07-15 13:00:27 +0200

    Batch Layer Integration v1

--- otclambda/conf/webinar_batchlayer.sql
+++ otclambda/conf/webinar_batchlayer.sql
@@ -1,14 +1,28 @@
 --- create time window to all name
-insert into lambda_data_markt
-select b.* , a.datewindows
-from (
-select from_unixtime( unix_timestamp(date_sub(current_date(), 1) ) +  (a.a + (10 * b.a) + (100 * c.a) ) * 300  )   AS datewindows
-from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
-cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
-cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
-) a 
-left outer join 
-( select *, from_unixtime( unix_timestamp( messagedate )  - pmod(  unix_timestamp( messagedate ),300 ) ) as windowmessagedate from inboxbatchlambda) b
-on  b.windowmessagedate = a.datewindows 
-where messageid is not null
-order by a.datewindows; 
+--insert into lambda_data_markt
+SELECT d.messageid, 
+       d.metrics, 
+       d.name, 
+       d.value, 
+       w.datewindows 
+from 
+    (
+    select from_unixtime( unix_timestamp(date_sub(current_date(), 1) ) +  (a.a + (10 * b.a) + (100 * c.a) ) * 300  ) AS datewindows
+    from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
+    cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
+    cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
+    order by datewindows   
+    ) w
+left outer join
+    ( select 
+        messageid, 
+        from_unixtime( unix_timestamp( messagedate )  - pmod(  unix_timestamp( messagedate ),300 ) ) as windowmessagedate,
+        metrics,
+        name,
+        value  
+      from 
+        inboxbatchlambda) d
+on  d.windowmessagedate = w.datewindows
+--where messageid is not null
+;
+
